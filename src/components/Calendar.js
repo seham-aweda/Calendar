570f4439c680914a4af8d2dcf430978d39bc7411
seham-aweda <awedaseham@gmail.com>
2021-11-01 00:25:20 +0200

    board

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -4,21 +4,24 @@ import Board from "./board";
 const Calendar=()=>{
     const tim=new Date()
     const months=['JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER']
-    const [SelectedMonth,setSelectedMonth]=React.useState("")
+    const [SelectedMonth,setSelectedMonth]=React.useState(months[tim.getMonth()]+" "+tim.getFullYear())
 
     const ChangeDate=(e)=>{
         setSelectedMonth(e.target.value)
     }
     return (
         <div>
+            {console.log(tim.getMonth()+1)}
             <select onChange={ChangeDate}>
-                <option>Any</option>
                 {months.map(u=>{
-                return <option>{u+" "+tim.getFullYear()}</option>
+                    if(u===months[tim.getMonth()])
+                        return <option selected> {u+" "+tim.getFullYear()}</option>
+                    else
+                        return <option>{u+" "+tim.getFullYear()}</option>
             })}
             </select>
             <h1>{SelectedMonth}</h1>
-<Board month={months.indexOf(SelectedMonth.slice(0,SelectedMonth.length-5))+1}/>
+            <Board month={months.indexOf(SelectedMonth.slice(0,SelectedMonth.length-5))+1}/>
 
         </div>
     )
--- src/components/board.js
+++ src/components/board.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-    const tim=new Date()
-const Board = ({month=(Number(tim.getMonth())+1)}) =>{
-
+const Board = ({month}) =>{
     const squares=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41]
-    const[dat,setdat]=React.useState("")
-            const start = new Date(`${month} 1, 2021`);
-               console.log(start.getDay());
-    //            const last=new Date(`November 30,2021`)
-    // console.log(last.getDay())
-    var d = new Date(2021, month, 0);
-    console.log(d.getDate())
+    const [dat,setdat]=React.useState("")
+    const start = new Date(`${month} 1, 2021`);
+         console.log(start.getDay());
+    const d = new Date(2021, month, 0);
+         console.log(d.getDate())
     const LastMon=squares.slice(0,start.getDay())
     const CurrMon=squares.slice(start.getDay(),start.getDay()+d.getDate())
     const NextMon=squares.slice(start.getDay()+d.getDate())
@@ -25,24 +21,18 @@ const Board = ({month=(Number(tim.getMonth())+1)}) =>{
                 <div>Thursday</div>
                 <div>Friday</div>
                 <div>Saturday</div>
-                {/*{squares.map((square) => {*/}
-                {/*    return <div id={square} key={square}>{square}</div>*/}
-                {/*})*/}
 
                 {LastMon.map(u => {
-                    return <div className={'square'} key={u} style={{}}>0</div>
+                    return <div className={'square'} key={u} style={{visibility:"hidden"}}></div>
                 })}
                 { CurrMon.map(u=>{
-                    return <div className={'square'} key={u}>{u-(start.getDay()-1)}</div>
+                    return <div className={'square'} key={u}><span className={"circle"}>{u-(start.getDay()-1)}</span></div>
                 })}
                 {NextMon.map(u=>{
-                    return <div className={'square'} key={u}>0</div>
-                })
+                    return <div className={'square'} key={u} style={{visibility:"hidden"}}></div>
+                })}
 
 
-
-
-                }
             </div>
         </div>
     )
