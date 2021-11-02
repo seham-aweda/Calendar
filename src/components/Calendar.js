763a88fe7fb052b1f92473ab97c14552bdcc2d66
seham-aweda <awedaseham@gmail.com>
2021-11-02 16:25:38 +0200

    board

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,28 +1,106 @@
 import React from "react";
 import Board from "./board";
+import axios from 'axios';
 
 const Calendar=()=>{
     const tim=new Date()
     const months=['JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER']
     const [SelectedMonth,setSelectedMonth]=React.useState(months[tim.getMonth()]+" "+tim.getFullYear())
+    const [users,setUsers]=React.useState([])
+    const [OneUser,setOneUser]=React.useState("")
+    const [Password,setPassword]=React.useState("")
+    const [TempUser,setTempUser]=React.useState("")
+    const [CurrentID,setCurrentID]=React.useState("")
+    React.useEffect(()=>{
+        GetName()
+    },[])
+
 
     const ChangeDate=(e)=>{
         setSelectedMonth(e.target.value)
     }
-    return (
-        <div>
-            {console.log(tim.getMonth()+1)}
-            <select onChange={ChangeDate}>
-                {months.map(u=>{
-                    if(u===months[tim.getMonth()])
-                        return <option selected> {u+" "+tim.getFullYear()}</option>
-                    else
-                        return <option>{u+" "+tim.getFullYear()}</option>
-            })}
-            </select>
-            <h1>{SelectedMonth}</h1>
-            <Board month={months.indexOf(SelectedMonth.slice(0,SelectedMonth.length-5))+1}/>
 
+    const GetName=async()=>{
+        let result=await axios.get('https://617eae9a2ff7e600174bd8b5.mockapi.io/User')
+        if(result.status === 200)
+            setUsers(result.data)
+    }
+
+    const OnChangeInput=(e)=>{
+        if(e.target.name === 'OneUser'){
+                setOneUser(e.target.value)
+        }
+        else if(e.target.name === 'Password'){
+                setPassword(e.target.value)
+        }
+    }
+    const AddUser=async()=> {
+        if(OneUser!=="" && Password!==""){
+        let copyUsers=[...users]
+      let found=copyUsers.find(u=>{
+          if(u.name===OneUser && u.password===Password){
+              return u
+          }
+      })
+      console.log(found)
+        // console.log(found===undefined)
+        if(found!==undefined){
+            setTempUser(found.name)
+            setCurrentID(found.id)
+        }
+        else if(found===undefined){
+            let NewUser={
+                name:OneUser,
+                password:Password,
+            }
+            let res=await axios.post("https://617eae9a2ff7e600174bd8b5.mockapi.io/User" ,NewUser)
+            console.log(res)
+            let copyUsers=[...users]
+            copyUsers.push(res.data)
+            setUsers(copyUsers)
+            setTempUser(OneUser)
+            setCurrentID(res.data.id)
+            setOneUser("")
+            setPassword("")
+        }}
+        else{
+            alert('Fill Out All The Fields')
+        }
+    }
+ const LogOut=()=>{
+        setTempUser("")
+     setOneUser("")
+     setPassword("")
+ }
+    return (
+
+        <div>
+            {console.log(users)}
+            {TempUser === "" ?
+                <>
+                <input type={"text"} placeholder={'UserName'} required value={OneUser} name={'OneUser'} onChange={OnChangeInput}/>
+                <input type={"text"} placeholder={'PassWord'} required value={Password} name={'Password'} onChange={OnChangeInput}/>
+                <input type={"button"} value={"Log In"} onClick={AddUser}/>
+                </> :
+                <>
+            {console.log(tim.getMonth()+1)}
+                    <div style={{display:"flex",justifyContent: 'space-around'}}>
+                        <h3 style={{height:'fit-content',margin:'auto'}}>Welcome Back Dear {TempUser}</h3>
+                        <button onClick={LogOut} style={{height:'fit-content',margin:'auto'}}><span></span>Log Out</button>
+                    </div>
+                    <br/>
+                <select onChange={ChangeDate}>
+                <option >Pick A Month</option>
+            {months.map(u=>{
+                if(u===months[tim.getMonth()])
+                return <option key={u} defaultValue> {u+" "+tim.getFullYear()}</option>
+                else
+                return <option key={u}>{u+" "+tim.getFullYear()}</option>
+            })}
+                </select>
+                <h1>{SelectedMonth}</h1>
+                <Board IDS={CurrentID} month={months.indexOf(SelectedMonth.slice(0,SelectedMonth.length-5))+1} year={tim.getFullYear()}/>
+                </> }
         </div>
     )
 }
--- src/components/board.js
+++ src/components/board.js
@@ -1,15 +1,74 @@
 import React from 'react';
+import axios from "axios";
 
-const Board = ({month}) =>{
+const Board = ({month,year,IDS}) =>{
+    const colors=['black','blueviolet','blue','aquamarine','brown','cadetblue','chartreuse','chocolate','coral','darkblue','darkorange','darkred','darksalmon','deeppink','fuchsia','gold','lightgreen','purple','teal']
+    let RanIndex=Math.floor(Math.random()*colors.length)
+    console.log('RanIndex',RanIndex)
     const squares=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41]
-    const [dat,setdat]=React.useState("")
-    const start = new Date(`${month} 1, 2021`);
+    const [ClickedDate,setClickedDate]=React.useState("")
+    const [showForm,setShowForm]=React.useState(false)
+    const [Tasks,setTasks]=React.useState([])
+    const [TempTask,setTempTask]=React.useState('')
+    const [TempHour,setTempHour]=React.useState('')
+
+
+    const start = new Date(`${month} 1, ${year}`);
          console.log(start.getDay());
-    const d = new Date(2021, month, 0);
+    const d = new Date(year, month, 0);
          console.log(d.getDate())
     const LastMon=squares.slice(0,start.getDay())
     const CurrMon=squares.slice(start.getDay(),start.getDay()+d.getDate())
     const NextMon=squares.slice(start.getDay()+d.getDate())
+
+    React.useEffect(()=>{
+       GetTasks()
+    },[])
+
+    const GetTasks=async()=>{
+        let result=await axios.get(`https://617eae9a2ff7e600174bd8b5.mockapi.io/User/`+IDS+`/Agenda`)
+        if(result.status === 200)
+            setTasks(result.data)
+    }
+
+    const ClickToShowForm=(e)=>{
+             if(Number(e.target.textContent)<10){
+                 setClickedDate('0'+e.target.textContent)
+             }else{
+        setClickedDate(e.target.textContent)
+             }
+        setShowForm(true)
+    }
+
+    const TaskInput=(e)=>{
+        if(e.target.name==='sub'){
+            setTempTask(e.target.value)
+        }
+        if(e.target.name==='appointment'){
+            setTempHour(e.target.value)
+        }
+    }
+
+    const AddTask=async()=>{
+        if(TempTask === "" && TempHour === ""){
+            alert('Fill it or press cancel')}
+        else{
+            let NewTask={
+                taskTitle:TempTask,
+                time:TempHour,
+                day:ClickedDate
+            }
+            let res=await axios.post(`https://617eae9a2ff7e600174bd8b5.mockapi.io/User/`+IDS+`/Agenda` ,NewTask)
+            let copyTask=[...Tasks]
+            copyTask.push(res.data)
+            setTasks(copyTask)
+            setClickedDate('')
+            setShowForm(false)
+            setTempTask('')
+            setTempHour('')
+        }
+    }
+
 
     return (
         <div>
@@ -23,17 +82,36 @@ const Board = ({month}) =>{
                 <div>Saturday</div>
 
                 {LastMon.map(u => {
-                    return <div className={'square'} key={u} style={{visibility:"hidden"}}></div>
+                    return <div className={'square'} key={u} style={{opacity:"0.5"}}></div>
                 })}
                 { CurrMon.map(u=>{
-                    return <div className={'square'} key={u}><span className={"circle"}>{u-(start.getDay()-1)}</span></div>
+                    return <div onClick={ClickToShowForm} id={u-(start.getDay()-1)} className={'square'} key={u}>{u-(start.getDay()-1)}
+                        {Tasks.length===0?"":Tasks.map(t=>{
+                            if(t.day===(u-(start.getDay()-1))){
+                            return <span style={{border:'1px solid black',borderRadius:'50%',backgroundColor:`${colors[RanIndex]}`,width:"10px",height:"10px"}}></span>
+                        }})}
+                    </div>
                 })}
                 {NextMon.map(u=>{
-                    return <div className={'square'} key={u} style={{visibility:"hidden"}}></div>
+                    return <div className={'square'} key={u} style={{opacity:"0.5"}}></div>
                 })}
-
-
             </div>
+            {showForm ?
+                <div>
+                    {console.log(ClickedDate)}
+                    <br/>
+                    <label>Date:
+                        <input readOnly type="date" name="date"  value={`${year}-${month}-${ClickedDate}`}  min={`${year}-${month}-${ClickedDate}`} max={`${year}-${month}-${ClickedDate}`}/></label>
+                    <br/>
+                    <label>Sub:
+                        <input type="text" name="sub" required onChange={TaskInput}/></label>
+                    <br/>
+                    <label htmlFor="appointment">Choose an appointment time: </label>
+                    <input id="appointment" type="time" name="appointment" onChange={TaskInput} />
+                    <br/>
+                    <input type={"button"} value={"ADD"} onClick={AddTask}/>
+
+                </div>:"Click on the day to add your appointment"}
         </div>
     )
 }
