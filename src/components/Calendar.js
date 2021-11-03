b6aee836df45b851a46b1d0cc49f72f46bb0fdd0
seham-aweda <awedaseham@gmail.com>
2021-11-04 01:25:19 +0200

    FINAL

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Board from "./board";
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Calendar=()=>{
     const tim=new Date()
+    const year = tim.getFullYear();
+    const years = Array.from(new Array(20),( val, index) => index + year)
     const months=['JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER']
-    const [SelectedMonth,setSelectedMonth]=React.useState(months[tim.getMonth()]+" "+tim.getFullYear())
+    const [SelectedMonth,setSelectedMonth]=React.useState(months[tim.getMonth()])
+    const [SelectedYear,setSelectedYear]=React.useState(year)
     const [users,setUsers]=React.useState([])
     const [OneUser,setOneUser]=React.useState("")
     const [Password,setPassword]=React.useState("")
     const [TempUser,setTempUser]=React.useState("")
+    const [FriendUser,setFriendUser]=React.useState("")
     const [CurrentID,setCurrentID]=React.useState("")
+    const [FriendID,setFriendID]=React.useState("")
+    const [ShowMe,setShowMe]=React.useState(true)
     React.useEffect(()=>{
         GetName()
     },[])
@@ -18,6 +26,9 @@ const Calendar=()=>{
 
     const ChangeDate=(e)=>{
         setSelectedMonth(e.target.value)
+    }
+    const ChangeYear=(e)=>{
+        setSelectedYear(e.target.value)
     }
 
     const GetName=async()=>{
@@ -35,23 +46,39 @@ const Calendar=()=>{
         }
     }
     const AddUser=async()=> {
-        if(OneUser!=="" && Password!==""){
         let copyUsers=[...users]
+
+        if(OneUser!=="" && Password!==""){
+
       let found=copyUsers.find(u=>{
-          if(u.name===OneUser && u.password===Password){
+          if(u.name===OneUser){
               return u
           }
       })
       console.log(found)
         // console.log(found===undefined)
-        if(found!==undefined){
+        if(found!==undefined && found.password===Password){
+
             setTempUser(found.name)
             setCurrentID(found.id)
         }
-        else if(found===undefined){
+        else  if(found!==undefined && found.password!==Password){
+             toast('This UserName Already Exist,Check Your Password',{
+                 position: "top-center",
+                 autoClose: 3000,
+                 hideProgressBar: false,
+                 closeOnClick: true,
+                 pauseOnHover: true,
+                 draggable: true,
+                 progress: undefined,
+             })
+        }
+        else if(found===undefined ){
+
             let NewUser={
                 name:OneUser,
                 password:Password,
+
             }
             let res=await axios.post("https://617eae9a2ff7e600174bd8b5.mockapi.io/User" ,NewUser)
             console.log(res)
@@ -62,45 +89,118 @@ const Calendar=()=>{
             setCurrentID(res.data.id)
             setOneUser("")
             setPassword("")
+            setShowMe(true)
+
         }}
         else{
-            alert('Fill Out All The Fields')
+            toast('Fill Out All The Fields',{
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
         }
     }
  const LogOut=()=>{
         setTempUser("")
      setOneUser("")
      setPassword("")
+     setShowMe(true)
+
+ }
+
+ const ChangeFriend=(e)=>{
+     setShowMe(true)
+        console.log(e.target.value)
+             let friend = users.find(user => {
+     if(e.target.value!='Any') {
+                 if (user.name === e.target.value) {
+                     return user
+                 }
+     }
+             })
+                 console.log(friend)
+             setFriendID(friend.id)
+         console.log(ShowMe)
+             setShowMe(false)
+         setFriendUser(friend.name)
+ e.target.value='Any'
+ }
+ const Back=()=>{
+     setShowMe(true)
+     setFriendID('')
+     setFriendUser('')
+
  }
     return (
 
         <div>
             {console.log(users)}
-            {TempUser === "" ?
+            {console.log(ShowMe)
+            }            {TempUser === "" ?
                 <>
+                    <div style={{display:"flex"}}>
+                    <div><img src={"https://www.pixelstalk.net/wp-content/uploads/images5/September-2021-Calendar-Wallpaper-HD.jpg"} width={"70%"} height={"70%"}/></div>
+                    <div style={{margin:'auto'}}>
                 <input type={"text"} placeholder={'UserName'} required value={OneUser} name={'OneUser'} onChange={OnChangeInput}/>
                 <input type={"text"} placeholder={'PassWord'} required value={Password} name={'Password'} onChange={OnChangeInput}/>
-                <input type={"button"} value={"Log In"} onClick={AddUser}/>
-                </> :
+                <input type={"button"} value={"Log In"} onClick={AddUser} className={'btn'}/>
+                </div></div> </> :
                 <>
             {console.log(tim.getMonth()+1)}
                     <div style={{display:"flex",justifyContent: 'space-around'}}>
-                        <h3 style={{height:'fit-content',margin:'auto'}}>Welcome Back Dear {TempUser}</h3>
-                        <button onClick={LogOut} style={{height:'fit-content',margin:'auto'}}><span></span>Log Out</button>
+                        <h3 style={{height:'fit-content',margin:'auto'}}>Welcome Back Dear {ShowMe?TempUser:FriendUser}</h3>
+                        <button onClick={LogOut} style={{height:'fit-content',margin:'auto'}} className={'btn'}><span></span>Log Out</button>
                     </div>
-                    <br/>
+
                 <select onChange={ChangeDate}>
                 <option >Pick A Month</option>
             {months.map(u=>{
                 if(u===months[tim.getMonth()])
-                return <option key={u} defaultValue> {u+" "+tim.getFullYear()}</option>
+                return <option key={u} defaultValue> {u}</option>
                 else
-                return <option key={u}>{u+" "+tim.getFullYear()}</option>
+                return <option key={u}>{u}</option>
             })}
                 </select>
-                <h1>{SelectedMonth}</h1>
-                <Board IDS={CurrentID} month={months.indexOf(SelectedMonth.slice(0,SelectedMonth.length-5))+1} year={tim.getFullYear()}/>
-                </> }
+                    <select onChange={ChangeYear}>
+                <option >Pick A Year</option>
+            {years.map(u=>{
+                if(u===year)
+                return <option key={u} defaultValue> {u}</option>
+                else
+                return <option key={u}>{u}</option>
+            })}
+                </select>
+                    <br/>
+                    <div className={"main"}>
+                        <div style={{display:"flex",backgroundColor:'lightgray'}}>
+                        <hr style={{marginLeft:'20px',backgroundColor:'black'}}/>
+                        <div><div>
+                            <h1 style={{color:'#daa94a',padding:"10px"}}><p style={{color:'black'}}>{months.indexOf(SelectedMonth)+1}</p>{SelectedMonth}<p>{SelectedYear}</p></h1>
+                          <h3>let's check some FRIENDS schedule</h3>
+                            {ShowMe?<select onChange={ChangeFriend} style={{backgroundColor:'transparent',border:'none'}}>
+                               <option>Any</option>
+                                {users.map(user=>{
+                                    if(user.name!==TempUser) {
+                                        return <option id={user.id}
+                                                       style={{backgroundColor: '#D3D3D3'}}>{user.name}</option>
+                                    }})
+                                }
+                            </select>:""}
+                            <input style={{backgroundColor: 'transparent',border:'none'}} type={"button"} value={"Back To Mine"} onClick={Back}/>
+                        </div></div></div>
+                        <div>
+                            {ShowMe?
+                                <Board IDS={CurrentID} month={months.indexOf(SelectedMonth) + 1} year={SelectedYear}/>
+                               :""}
+                            {!ShowMe ?
+                                <Board IDS={FriendID} month={months.indexOf(SelectedMonth) + 1} year={SelectedYear}/>
+                                    : "" }
+                        </div> </div></> }
+            <ToastContainer />
         </div>
     )
 }
--- src/components/board.js
+++ src/components/board.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Board = ({month,year,IDS}) =>{
-    const colors=['black','blueviolet','blue','aquamarine','brown','cadetblue','chartreuse','chocolate','coral','darkblue','darkorange','darkred','darksalmon','deeppink','fuchsia','gold','lightgreen','purple','teal']
-    let RanIndex=Math.floor(Math.random()*colors.length)
-    console.log('RanIndex',RanIndex)
     const squares=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41]
     const [ClickedDate,setClickedDate]=React.useState("")
     const [showForm,setShowForm]=React.useState(false)
     const [Tasks,setTasks]=React.useState([])
     const [TempTask,setTempTask]=React.useState('')
     const [TempHour,setTempHour]=React.useState('')
+    const [isUpdate, setIsUpdate] = React.useState(false)
+    const [updateId, setUpdateId] = React.useState(-1)
 
 
     const start = new Date(`${month} 1, ${year}`);
@@ -31,15 +32,32 @@ const Board = ({month,year,IDS}) =>{
             setTasks(result.data)
     }
 
-    const ClickToShowForm=(e)=>{
-             if(Number(e.target.textContent)<10){
-                 setClickedDate('0'+e.target.textContent)
-             }else{
-        setClickedDate(e.target.textContent)
-             }
-        setShowForm(true)
-    }
+    const ClickToShowForm=(e)=> {
+        setShowForm(false)
+        if (month < (new Date().getMonth())+1) {
+                toast('It\'s Too Late, You Can\'t Go Back In Time',{
+                    position: "top-center",
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                })
+            }
+        // if(e.target.id < (new Date().getDay())){
+        //         toast('It\'s Too Late, You Can\'t Go Back In Time')
+        // }
 
+        else {
+            if (Number(e.target.id) < 10) {
+                setClickedDate('0' + e.target.id)
+            } else {
+                setClickedDate(e.target.id)
+            }
+            setShowForm(true)
+        }
+    }
     const TaskInput=(e)=>{
         if(e.target.name==='sub'){
             setTempTask(e.target.value)
@@ -51,12 +69,22 @@ const Board = ({month,year,IDS}) =>{
 
     const AddTask=async()=>{
         if(TempTask === "" && TempHour === ""){
-            alert('Fill it or press cancel')}
+            toast('Fill it or press cancel',{
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })}
         else{
             let NewTask={
                 taskTitle:TempTask,
                 time:TempHour,
-                day:ClickedDate
+                day:ClickedDate,
+                month:month,
+                year:year
             }
             let res=await axios.post(`https://617eae9a2ff7e600174bd8b5.mockapi.io/User/`+IDS+`/Agenda` ,NewTask)
             let copyTask=[...Tasks]
@@ -68,27 +96,91 @@ const Board = ({month,year,IDS}) =>{
             setTempHour('')
         }
     }
+    const Cancel=()=>{
+        setClickedDate('')
+        setShowForm(false)
+        setIsUpdate(false)
+        setTempTask('')
+        setTempHour('')
+    }
+    const DeleteTask=async(val)=>{
+             let res=await axios.delete(`https://617eae9a2ff7e600174bd8b5.mockapi.io/User/`+IDS+`/Agenda`+`/`+val)
+        setShowForm(false)
+        if(res.status===200){
+        let copyTasks=[...Tasks]
+        let result=copyTasks.filter((task)=>{
+            if(task.id!==val)
+                return task
+        })
+        setTasks(result)
+       }}
 
+    const EditTask=(id)=>{
+        setIsUpdate(true);
+        let t = Tasks.find(value => {
+            return value.id === id
+        })
+        setTempTask(t.taskTitle)
+        setTempHour(t.time)
+        setClickedDate(t.day)
+        setUpdateId(t.id);
+        console.log('ffff',ClickedDate)
+         }
 
+    const updateNewTaskHandler = async () => {
+        let response = await axios.put(`https://617eae9a2ff7e600174bd8b5.mockapi.io/User/`+IDS+`/Agenda`+`/`+updateId,{
+            taskTitle : TempTask,
+            time:TempHour,
+        })
+        if(response.status === 200){
+            const TasksList = [...Tasks];
+            let tas = TasksList.find(value => {
+                return value.id === updateId
+            })
+            tas.taskTitle =TempTask
+            tas.time=TempHour
+
+            setTasks(TasksList)
+        }
+        console.log("response :",response)
+        setShowForm(false)
+        setIsUpdate(false)
+        setTempTask("")
+        setTempHour('')
+    }
     return (
         <div>
+            {console.log(Tasks)}
             <div className={"grid"}>
-                <div>Sunday</div>
-                <div>Monday</div>
-                <div>Tuesday</div>
-                <div>Wednesday</div>
-                <div>Thursday</div>
-                <div>Friday</div>
-                <div>Saturday</div>
+                <div className={"header"}>Sunday</div>
+                <div className={"header"}>Monday</div>
+                <div className={"header"}>Tuesday</div>
+                <div className={"header"}>Wednesday</div>
+                <div className={"header"}>Thursday</div>
+                <div className={"header"}>Friday</div>
+                <div className={"header"}>Saturday</div>
 
                 {LastMon.map(u => {
                     return <div className={'square'} key={u} style={{opacity:"0.5"}}></div>
                 })}
-                { CurrMon.map(u=>{
-                    return <div onClick={ClickToShowForm} id={u-(start.getDay()-1)} className={'square'} key={u}>{u-(start.getDay()-1)}
+                { CurrMon.map(u=>{return <div onClick={ClickToShowForm} id={u-(start.getDay()-1)} className={'square'} key={u}>{u-(start.getDay()-1)}
                         {Tasks.length===0?"":Tasks.map(t=>{
-                            if(t.day===(u-(start.getDay()-1))){
-                            return <span style={{border:'1px solid black',borderRadius:'50%',backgroundColor:`${colors[RanIndex]}`,width:"10px",height:"10px"}}></span>
+                            if(t.day==(u-(start.getDay()-1))&& t.month==month && t.year==year){
+                            return <span  className={'tooltip'} style={{width:'fit-content',border:`0.5rem solid ${t.color}`,borderRadius:'100%',margin:"3px"}}>
+                                <span  className={"tooltiptext"} id={t.day} ><span onClick={()=>{EditTask(t.id)}}><svg xmlns="http://www.w3.org/2000/svg"
+                                                                             width="20" height="20" fill="currentColor"
+                                                                             className="bi bi-pencil-square"
+                                                                             viewBox="0 0 16 16">
+  <path
+      d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fillRule="evenodd"
+        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg>    </span><span onClick={()=>{DeleteTask(t.id)}}><svg
+                                    xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                    className="bi bi-eraser-fill" viewBox="0 0 16 16">
+  <path
+      d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm.66 11.34L3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z"/>
+</svg>  </span>  {t.taskTitle} at {t.time} </span></span>
                         }})}
                     </div>
                 })}
@@ -100,18 +192,25 @@ const Board = ({month,year,IDS}) =>{
                 <div>
                     {console.log(ClickedDate)}
                     <br/>
-                    <label>Date:
+                    {!isUpdate?
+                    <label>Date :
                         <input readOnly type="date" name="date"  value={`${year}-${month}-${ClickedDate}`}  min={`${year}-${month}-${ClickedDate}`} max={`${year}-${month}-${ClickedDate}`}/></label>
+                    :""}
+                        <br/>
+                    <label>Sub :
+                        <input type="text" name="sub" value={TempTask} required onChange={TaskInput}/></label>
                     <br/>
-                    <label>Sub:
-                        <input type="text" name="sub" required onChange={TaskInput}/></label>
+                    <label htmlFor="appointment">Choose an appointment time :  </label>
+                    <input id="appointment" type="time" value={TempHour} name="appointment" onChange={TaskInput} />
                     <br/>
-                    <label htmlFor="appointment">Choose an appointment time: </label>
-                    <input id="appointment" type="time" name="appointment" onChange={TaskInput} />
-                    <br/>
-                    <input type={"button"} value={"ADD"} onClick={AddTask}/>
+                    {!isUpdate?
+                    <input type={"button"} value={"ADD"} onClick={AddTask} className={'add'}/>:''}
+                    {isUpdate?
+                    <input type={"button"} value={"Update"} onClick={updateNewTaskHandler} className={'add'}/>:''}
+                    <input type={"button"} value={"Cancel"} onClick={Cancel} className={'add'}/>
 
                 </div>:"Click on the day to add your appointment"}
+
         </div>
     )
 }
